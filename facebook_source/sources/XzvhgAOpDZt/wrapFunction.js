857138fa3474dad17a916d9e43b18027f586a461
ustbhuangyi <280309453@qq.com>
2014-04-02 10:04:17 +0800

    reflect wrapFunction.js
    finish

--- facebook_source/sources/XzvhgAOpDZt/wrapFunction.js
+++ facebook_source/sources/XzvhgAOpDZt/wrapFunction.js
@@ -1,16 +1,30 @@
-__d("wrapFunction", [], function(global /*a*/ , require /*b*/ , requireDynamic /*c*/ , requireLazy /*d*/ , module /*e*/ , exports /*f*/ ) {
-    var g = {};
+/*函数包装器 */
+__d("wrapFunction", [], function (global, require, requireDynamic, requireLazy, module, exports) {
+    var wrapMap = {}; //包装器Map
 
-    function h(i, j, k) {
-        j = j || 'default';
-        return function() {
-            var l = j in g ? g[j](i, k) : i;
-            return l.apply(this, arguments);
+    /*
+     * 用某个函数包装器去包装函数
+     * @param fn {Function} 需要被包装的函数
+     * @param key {String} 函数包装器的key
+     * @param args {Object} 执行包装时的参数
+    */
+    function wrapFunction(fn, key, args) {
+        key = key || 'default';
+        return function () {
+            var fn = key in fnMap ? wrapMap[key](fn, args) : fn;
+            return fn.apply(this, arguments);
         };
     }
-    h.setWrapper = function(i, j) {
-        j = j || 'default';
-        g[j] = i;
+    
+    /*
+     * 设置一个包装函数,添加到map中
+     * @param wrap {Function} 包装函数
+     * @param key {String} 包装函数的key
+    */
+    wrapFunction.setWrapper = function (wrap, key) {
+        key = key || 'default';
+        wrapMap[key] = wrap;
     };
-    module /*e*/ .exports = h;
+
+    module.exports = wrapFunction;
 });
